586eb9d46f81cf74f23a23e28d0b6ba1a79377ad
EdwarsBosarreyes <bosaed07@gmail.com>
2026-06-23 21:26:24 -0600

    Reto 5: done

--- tests/navigation.spec.ts
+++ tests/navigation.spec.ts
@@ -108,3 +108,48 @@ test("Check all the qualification links", async ({ page }) => {
     await page.getByRole("navigation", { name: "Topbar Menu" }).getByText("Qualifications").click();
   }
 });
+
+test("Check all the job links", async ({ page }) => {
+  const expectedPages = [
+    {
+      menu: "Job Titles",
+      url: "/web/index.php/admin/viewJobTitleList",
+    },
+    {
+      menu: "Pay Grades",
+      url: "/web/index.php/admin/viewPayGrades",
+    },
+    {
+      menu: "Employment Status",
+      url: "/web/index.php/admin/employmentStatus",
+    },
+    {
+      menu: "Job Categories",
+      url: "/web/index.php/admin/jobCategory",
+    },
+    {
+      menu: "Work Shifts",
+      url: "/web/index.php/admin/workShift",
+    },
+  ];
+  await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+  await page.getByRole("textbox", { name: "Username" }).fill("Admin");
+  await page.getByRole("textbox", { name: "Password" }).fill("admin123");
+  await page.getByRole("button", { name: "Login" }).click();
+
+  await expect(page.getByRole("link", { name: "Admin" })).toBeVisible();
+
+  await page.getByRole("link", { name: "Admin" }).click();
+
+  await page.getByRole("navigation", { name: "Topbar Menu" }).getByText("Job").click();
+
+  const qualificationOptions = page.getByRole("menu").locator("li");
+
+  for (let expectedPage of expectedPages) {
+    const menuOption = qualificationOptions.filter({ hasText: expectedPage.menu });
+    await menuOption.click();
+    await expect(page).toHaveURL(new RegExp(expectedPage.url));
+
+    await page.getByRole("navigation", { name: "Topbar Menu" }).getByText("Job").click();
+  }
+});
