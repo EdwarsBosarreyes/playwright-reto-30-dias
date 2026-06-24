72fc581ae43d0dfae8d0337852212ed7e9c73ba3
EdwarsBosarreyes <bosaed07@gmail.com>
2026-06-23 21:13:46 -0600

    Adding navigation into Qualifications menu

--- tests/navigation.spec.ts
+++ tests/navigation.spec.ts
@@ -63,3 +63,48 @@ test("Navigate through the left panel", async ({ page }) => {
     }
   }
 });
+
+test("Check all the qualification links", async ({ page }) => {
+  const expectedPages = [
+    {
+      menu: "Skills",
+      url: "/web/index.php/admin/viewSkills",
+    },
+    {
+      menu: "Education",
+      url: "/web/index.php/admin/viewEducation",
+    },
+    {
+      menu: "Licenses",
+      url: "/web/index.php/admin/viewLicenses",
+    },
+    {
+      menu: "Languages",
+      url: "/web/index.php/admin/viewLanguages",
+    },
+    {
+      menu: "Memberships",
+      url: "/web/index.php/admin/membership",
+    },
+  ];
+  await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+  await page.getByRole("textbox", { name: "Username" }).fill("Admin");
+  await page.getByRole("textbox", { name: "Password" }).fill("admin123");
+  await page.getByRole("button", { name: "Login" }).click();
+
+  await expect(page.getByRole("link", { name: "Admin" })).toBeVisible();
+
+  await page.getByRole("link", { name: "Admin" }).click();
+
+  await page.getByRole("navigation", { name: "Topbar Menu" }).getByText("Qualifications").click();
+
+  const qualificationOptions = page.getByRole("menu").locator("li");
+
+  for (let expectedPage of expectedPages) {
+    const menuOption = qualificationOptions.filter({ hasText: expectedPage.menu });
+    await menuOption.click();
+    await expect(page).toHaveURL(new RegExp(expectedPage.url));
+
+    await page.getByRole("navigation", { name: "Topbar Menu" }).getByText("Qualifications").click();
+  }
+});
