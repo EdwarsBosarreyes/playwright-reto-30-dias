71bfbd8f0638076f9c525f568b95e56034f9f5cc
EdwarsBosarreyes <bosaed07@gmail.com>
2026-06-29 21:56:31 -0600

    Reto 6: done

--- tests/users.spec.ts
+++ tests/users.spec.ts
@@ -62,3 +62,53 @@ test("Select specific user for edition", async ({ page }) => {
     currentUsername,
   );
 });
+
+test("Select random user for edition", async ({ page }) => {
+  await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+  await page.getByRole("textbox", { name: "Username" }).fill("Admin");
+  await page.getByRole("textbox", { name: "Password" }).fill("admin123");
+  await page.getByRole("button", { name: "Login" }).click();
+
+  await expect(page.getByRole("link", { name: "Admin" })).toBeVisible();
+
+  await page.getByRole("link", { name: "Admin" }).click();
+  await page.getByRole("navigation", { name: "Topbar Menu" }).getByText("User Management").click();
+  await page.getByRole("menuitem", { name: "Users" }).click();
+
+  const rows = page.getByRole("table").getByRole("row");
+  const usernames: string[] = [];
+
+  const rowCount = await rows.count();
+
+  for (let i = 1; i < rowCount; i++) {
+    const cell = rows.nth(i).getByRole("cell").nth(1);
+    const username = await cell.textContent();
+
+    if (username && username != "Admin") {
+      usernames.push(username);
+    }
+  }
+
+  const randomIndex = Math.floor(Math.random() * usernames.length);
+
+  const randomUsername = usernames[randomIndex];
+
+  const pencilToEdit = page
+    .getByRole("table")
+    .getByRole("row")
+    .filter({ hasText: randomUsername })
+    .locator("button")
+    .filter({ has: page.locator("i.bi-pencil-fill") });
+
+  await pencilToEdit.click();
+
+  const currentUsername = await page
+    .locator("//label[contains(.,'Username')]/parent::div/following-sibling::div/input")
+    .inputValue();
+
+  expect(currentUsername).toEqual(randomUsername);
+
+  expect(page.locator("//label[contains(.,'Username')]/parent::div/following-sibling::div/input")).toHaveValue(
+    currentUsername,
+  );
+});
