d84dbd9ef8507dce3f03dbbc6172bfbb6e75fc3a
EdwarsBosarreyes <bosaed07@gmail.com>
2026-06-30 13:51:03 -0600

    Side Panel POM

---
+++ components/SidePanel.ts
@@ -0,0 +1,32 @@
+import { Page } from "@playwright/test";
+
+export class SidePanel {
+  readonly page: Page;
+
+  constructor(page: Page) {
+    this.page = page;
+  }
+
+  private menuOption(option: SideMenuOption) {
+    return this.page.getByRole("link", { name: option });
+  }
+
+  async clicOnOption(option: SideMenuOption) {
+    await this.menuOption(option).click();
+  }
+}
+
+export enum SideMenuOption {
+  ADMIN = "Admin",
+  PIM = "PIM",
+  LEAVE = "Leave",
+  TIME = "Time",
+  RECRUITMENT = "Recruitment",
+  MY_INFO = "My Info",
+  PERFORMANCE = "Performance",
+  DASHBOARD = "Dashboard",
+  DIRECTORY = "Directory",
+  MAINTENANCE = "Maintenance",
+  CLAIM = "Claim",
+  BUZZ = "Buzz",
+}
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,11 +1,15 @@
 import { expect, test } from "@playwright/test";
 import { LoginPage } from "../pageobjects/LoginPage";
+import { SideMenuOption, SidePanel } from "../components/SidePanel";
 
 test("Login to hrm", async ({ page }) => {
   const loginPage = new LoginPage(page);
   await loginPage.doLogin("Admin", "admin123");
 
-  await expect(page.getByRole("link", { name: "Admin" })).toBeVisible();
+  const sidePanel = new SidePanel(page);
+  await sidePanel.clicOnOption(SideMenuOption.ADMIN);
+  await sidePanel.clicOnOption(SideMenuOption.BUZZ);
+  await sidePanel.clicOnOption(SideMenuOption.DASHBOARD);
 });
 
 test("Invalid login to hrm", async ({ page }) => {
