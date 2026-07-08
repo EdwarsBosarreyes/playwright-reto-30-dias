76d51ca6bcc6c2da5e45c3a358806b886d4b9ff2
EdwarsBosarreyes <bosaed07@gmail.com>
2026-07-07 18:00:07 -0600

    Adding reuse of authentication

---
+++ tests/auth.setup.ts
@@ -0,0 +1,34 @@
+import { test as setup, expect } from "@playwright/test";
+import { LoginPage } from "../pageobjects/LoginPage";
+
+setup("aunthtentication as admin", async ({ page }) => {
+  console.log("Authetication started using setup");
+
+  //Login
+  const loginPage = new LoginPage(page);
+  await loginPage.loginAsAdmin();
+
+  //Assert for successfull login
+  await expect(page.getByRole("link", { name: "Admin" })).toBeVisible();
+
+  //Save status
+  await page.context().storageState({ path: ".auth/admin.json" });
+
+  console.log("Authentication completed using setup");
+});
+
+/*setup("aunthtentication as employee", async ({ page }) => {
+  console.log("Authetication started using setup");
+
+  //Login
+  const loginPage = new LoginPage(page);
+  await loginPage.loginAsEmployee();
+
+  //Assert for successfull login
+  await expect(page.getByRole("link", { name: "Admin" })).toBeVisible();
+
+  //Save status
+  await page.context().storageState({ path: ".auth/employee.json" });
+
+  console.log("Authentication completed using setup");
+}); */
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -2,43 +2,37 @@ import { expect, test } from "@playwright/test";
 import { LoginPage } from "../pageobjects/LoginPage";
 import { SideMenuOption, SidePanel } from "../components/SidePanel";
 
-test.describe("HRM Login and Navigation Tests", () => {
-  let loginPage: LoginPage;
-  let sidePanel: SidePanel;
+test("Login to hrm as Admin", async ({ page }) => {
+  await page.goto("/web/index.php/dashboard/index");
+  const sidePanel = new SidePanel(page);
 
-  test.beforeEach(async ({ page }) => {
-    loginPage = new LoginPage(page);
-    sidePanel = new SidePanel(page);
-  });
+  await sidePanel.clicOnOption(SideMenuOption.ADMIN);
+  await sidePanel.clicOnOption(SideMenuOption.BUZZ);
+  await sidePanel.clicOnOption(SideMenuOption.DASHBOARD);
+});
 
-  test("Login to hrm as Admin", async () => {
-    await loginPage.loginAsAdmin();
+test("Invalid login to hrm", async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  await loginPage.doLogin("Admin", "admin1234");
 
-    await sidePanel.clicOnOption(SideMenuOption.ADMIN);
-    await sidePanel.clicOnOption(SideMenuOption.BUZZ);
-    await sidePanel.clicOnOption(SideMenuOption.DASHBOARD);
-  });
+  await expect(page.getByRole("alert")).toBeVisible();
+});
 
-  test("Invalid login to hrm", async ({ page }) => {
-    const loginPage = new LoginPage(page);
-    await loginPage.doLogin("Admin", "admin1234");
+test("Search in the sidebar and clinking", async ({ page }) => {
+  await page.goto("/web/index.php/dashboard/index");
+  const sidePanel = new SidePanel(page);
 
-    await expect(page.getByRole("alert")).toBeVisible();
-  });
+  const randomOption = sidePanel.getRandomMenuOption();
+  await sidePanel.searchText(randomOption);
 
-  test("Search in the sidebar and clinking", async () => {
-    await loginPage.loginAsAdmin();
+  const searchedLink = sidePanel.getMenuOptionLocator(randomOption);
+  await expect(searchedLink).toHaveText(randomOption);
+  await searchedLink.click();
+});
 
-    const randomOption = sidePanel.getRandomMenuOption();
-    await sidePanel.searchText(randomOption);
-
-    const searchedLink = sidePanel.getMenuOptionLocator(randomOption);
-    await expect(searchedLink).toHaveText(randomOption);
-    await searchedLink.click();
-  });
-
-  test("Login to HRM as Employee", async () => {
-    await loginPage.loginAsEmployee();
-    await expect(sidePanel.getMenuOptionLocator(SideMenuOption.ADMIN)).toBeHidden();
-  });
+test("Login to HRM as Employee", async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  const sidePanel = new SidePanel(page);
+  //await loginPage.loginAsEmployee();
+  await expect(sidePanel.getMenuOptionLocator(SideMenuOption.ADMIN)).toBeHidden();
 });
