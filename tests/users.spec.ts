e3794ed2eafb3b2157f0564ad3a5538cb8955e66
EdwarsBosarreyes <bosaed07@gmail.com>
2026-07-14 22:43:38 -0600

    Add POM to AddNewUser flow

--- tests/users.spec.ts
+++ tests/users.spec.ts
@@ -2,6 +2,8 @@ import { expect, test } from "@playwright/test";
 import { getRandomElement } from "../src/utils/random";
 import { SideMenuOption, SidePanel } from "../components/SidePanel";
 import { TopBarMenu } from "../components/top-bar-menu/TopBarMenu";
+import { Navigate } from "../pageobjects/Navigate";
+import { AddNewUserPage } from "../pageobjects/AddNewUserPage";
 
 test("Get all the usernames registered", async ({ page }) => {
   await expect(page.getByRole("link", { name: "Admin" })).toBeVisible();
@@ -244,7 +246,8 @@ test("Add new user", async ({ page }) => {
   const password = "Random45..*";
   const employeeToSearch = "Qwerty LName";
 
-  await page.goto("/web/index.php/dashboard/index");
+  const navigate = new Navigate(page);
+  await navigate.toDashboard();
 
   const sidePanel = new SidePanel(page);
   await sidePanel.clicOnOption(SideMenuOption.ADMIN);
@@ -252,58 +255,16 @@ test("Add new user", async ({ page }) => {
   /* const topBarMenu = new TopBarMenu(page);
   await topBarMenu.userManagement.clickOnUsers(); */
 
-  await page.getByText("Add").click();
-
-  await page
-    .locator("div.oxd-grid-item--gutters")
-    .filter({
-      has: page.getByText("User Role"),
-    })
-    .locator("div.oxd-select-text-input")
-    .click();
-
-  await page.getByText("ESS", { exact: true }).click();
-
-  await page.getByRole("textbox", { name: "Type for hints..." }).fill(employeeToSearch);
-  await page.getByText("Qwerty Qwerty LName", { exact: true }).click();
-
-  await page
-    .locator("div.oxd-grid-item--gutters")
-    .filter({
-      has: page.getByText("Status"),
-    })
-    .locator("div.oxd-select-text-input")
-    .click();
-
-  await page.getByText("Enabled").click();
-
-  await page
-    .locator("div.oxd-grid-item--gutters")
-    .filter({
-      has: page.getByText("Username"),
-    })
-    .getByRole("textbox")
-    .fill(randomUsername);
-
-  await page
-    .locator("div.oxd-grid-item--gutters")
-    .filter({
-      has: page.getByText("Password", { exact: true }),
-    })
-    .getByRole("textbox")
-    .fill(password);
-
-  await page
-    .locator("div.oxd-grid-item--gutters")
-    .filter({
-      has: page.getByText("Confirm Password", { exact: true }),
-    })
-    .getByRole("textbox")
-    .fill(password);
-
-  await page.getByRole("button", { name: "Save" }).click();
-
-  await expect(page.locator("p.oxd-text--toast-message")).toHaveText("Successfully Saved");
+  const addNewUserPage = new AddNewUserPage(page);
+  await addNewUserPage.clickOnAdd();
+  await addNewUserPage.selectUserRole("ESS");
+  await addNewUserPage.selectEmployeeName(employeeToSearch);
+  await addNewUserPage.selectStatus("Enabled");
+  await addNewUserPage.enterUsername(randomUsername);
+  await addNewUserPage.enterPassword(password);
+  await addNewUserPage.enterConfirmPassword(password);
+  await addNewUserPage.clickOnSave();
+  await addNewUserPage.checkUserWasAddedMessage();
 });
 
 test("Add new user different confirm password", async ({ page }) => {
